b483a399cfba87cb327be9eb9d76058ec441ebd4
Dong-Ha Kim <dongha.kim210@gmail.com>
2017-10-09 15:35:05 +0200

    Add upvote and downvote buttons to post item

--- frontend/src/components/PostsList.js
+++ frontend/src/components/PostsList.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Item, Icon } from 'semantic-ui-react'
+import { Item, Icon, Statistic, Grid, Button } from 'semantic-ui-react'
 import dateformat from 'dateformat'
+
+import './App.css'
 
 class PostsList extends Component {
   render() {
@@ -12,6 +14,25 @@ class PostsList extends Component {
           posts
             ? posts.map(post => (
               <Item key={post.id}>
+                <Item.Image size='small'>
+                  <Grid columns={1}>
+                    <Grid.Row className='no-padding-bottom'>
+                      <Grid.Column textAlign='center'>
+                        <Statistic size='small'>
+                          <Statistic.Value>{post.voteScore}</Statistic.Value>
+                        </Statistic>
+                      </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row columns={2} className='no-padding-top'>
+                      <Grid.Column textAlign='right'>
+                        <Button inverted color='red' icon='thumbs down' />
+                      </Grid.Column>
+                      <Grid.Column>
+                        <Button inverted color='green' icon='thumbs up' />
+                      </Grid.Column>
+                    </Grid.Row>
+                  </Grid>
+                </Item.Image>
                 <Item.Content>
                   <Item.Header as='a'>{post.title}</Item.Header>
                   <Item.Meta>
